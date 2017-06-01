e56b88b08e4622509c49cd86e4a3a0450e92eb11
Chris Harvey <chrisharvey2pi@gmail.com>
2017-06-01 19:52:03 -0400

    minify and build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp')
+var rename = require('gulp-rename')
 var pug = require('gulp-pug')
 var less = require('gulp-less')
+var clean_css = require('gulp-clean-css')
 
 gulp.task('pug:docs', function () {
   return gulp.src('docs/{index,props}.pug')
@@ -26,4 +28,11 @@ gulp.task('lessc:docs', function () {
     .pipe(gulp.dest('docs/styles/'))
 })
 
-gulp.task('lessc:all', ['lessc:bangs', 'lessc:docs'])
+gulp.task('minify', ['lessc:bangs'], function () {
+  return gulp.src('bangs.css')
+    .pipe(clean_css())
+    .pipe(rename('bangs.min.css')) // TODO: use a SourceMap!
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('build', ['pug:docs', 'lessc:docs', 'minify'])
