7da9d358670841cdea00eadeb21926180a6c4b6d
Chris Harvey <chrisharvey2pi@gmail.com>
2017-07-07 16:49:11 -0400

    formatting fixups

--- _models/Bangs.class.js
+++ _models/Bangs.class.js
@@ -2,161 +2,160 @@ var Ajv = require('ajv')
 var Util = require('./Util.class.js')
 
 const DATA = (function compileData(data) {
-  // REVIEW indentation
-    /**
-     * NOTE: type definition
-     * A `TransformObj` object defines 3 nullable functions that determine how to transform a pure data value
-     * into a css `<value>` type (the type used to denote values of css properties).
-     * Each function takes 1 argument (of unknown type, depending on the function that requires it as a parameter),
-     * and returns a string.
-     * @typedef  {!Object} TransformObj
-     * @property {?function(?):string} TransformObj.namefn a function returning the value.name
-     * @property {?function(?):string} TransformObj.codefn a function returning the value.code
-     * @property {?function(?):string} TransformObj.usefn  a function returning the value.use
-     */
+  /**
+   * NOTE: type definition
+   * A `TransformObj` object defines 3 nullable functions that determine how to transform a pure data value
+   * into a css `<value>` type (the type used to denote values of css properties).
+   * Each function takes 1 argument (of unknown type, depending on the function that requires it as a parameter),
+   * and returns a string.
+   * @typedef  {!Object} TransformObj
+   * @property {?function(?):string} TransformObj.namefn a function returning the value.name
+   * @property {?function(?):string} TransformObj.codefn a function returning the value.code
+   * @property {?function(?):string} TransformObj.usefn  a function returning the value.use
+   */
 
-    /**
-     * Automate track fractions.
-     * Fractions can be percentages or any length unit, depending on the mixin passed.
-     * NOTE: WARNING: STATEFUL FUNCTION (uses `data` parameter above).
-     * NOTE: METHOD FUNCTION. This function uses `this`, so must be called on an object.
-     * @param  {TransformObj} transforms a set of possible transformations
-     * @param  {!Object={}} options a set of possible options
-     * @param  {(number|Array<number>|string)=1} options.domain if {number}, use 1–n tracks; if {Array}, use those entries; if {string}, get own property of `data.global.common_data`
-     */
-    function generateFracs(transforms, options={}) {
-      Util.arrayify(options.domain, data.global.common_data).forEach(function (den) {
-        for (let num = 1; num <= den; num++) {
-          let newvalue = {
-            name: `${Math.round(10000 * (num/den))/100}%`,
-            code: `${num}o${den}`,
-            use : (transforms.usefn) ? transforms.usefn.call(null, `(${num}/${den} * 100%)`) : '',
-          }
-          let value = this.values.find((v) => v.name===newvalue.name)
-          if (value) {
-            if (typeof value.code === 'string') {
-              value.code = [value.code]
-            }
-            value.code.push(newvalue.code)
-          } else {
-            this.values.push(newvalue)
-          }
+  /**
+   * Automate track fractions.
+   * Fractions can be percentages or any length unit, depending on the mixin passed.
+   * NOTE: WARNING: STATEFUL FUNCTION (uses `data` parameter above).
+   * NOTE: METHOD FUNCTION. This function uses `this`, so must be called on an object.
+   * @param  {TransformObj} transforms a set of possible transformations
+   * @param  {!Object={}} options a set of possible options
+   * @param  {(number|Array<number>|string)=1} options.domain if {number}, use 1–n tracks; if {Array}, use those entries; if {string}, get own property of `data.global.common_data`
+   */
+  function generateFracs(transforms, options={}) {
+    Util.arrayify(options.domain, data.global.common_data).forEach(function (den) {
+      for (let num = 1; num <= den; num++) {
+        let newvalue = {
+          name: `${Math.round(10000 * (num/den))/100}%`,
+          code: `${num}o${den}`,
+          use : (transforms.usefn) ? transforms.usefn.call(null, `(${num}/${den} * 100%)`) : '',
         }
-      }, this)
-    }
+        let value = this.values.find((v) => v.name===newvalue.name)
+        if (value) {
+          if (typeof value.code === 'string') {
+            value.code = [value.code]
+          }
+          value.code.push(newvalue.code)
+        } else {
+          this.values.push(newvalue)
+        }
+      }
+    }, this)
+  }
 
-    /**
-     * Automate counts.
-     * NOTE: WARNING: STATEFUL FUNCTION (uses `data` parameter above).
-     * NOTE: METHOD FUNCTION. This function uses `this`, so must be called on an object.
-     * @param  {TransformObj} transforms a set of possible transformations
-     * @param  {!Object={}} options a set of possible options
-     * @param  {(number|Array<number>|string)=1} options.domain if {number}, use 1–n tracks; if {Array}, use those entries; if {string}, get own property of `data.global.common_data`
-     * @param  {boolean=} options.negative if true, generate negative values as well (parallelling positive values)
-     */
-    function generateCounts(transforms, options={}) {
-      let arr = Util.arrayify(options.domain, data.global.common_data)
-      arr.forEach(function (ct) {
+  /**
+   * Automate counts.
+   * NOTE: WARNING: STATEFUL FUNCTION (uses `data` parameter above).
+   * NOTE: METHOD FUNCTION. This function uses `this`, so must be called on an object.
+   * @param  {TransformObj} transforms a set of possible transformations
+   * @param  {!Object={}} options a set of possible options
+   * @param  {(number|Array<number>|string)=1} options.domain if {number}, use 1–n tracks; if {Array}, use those entries; if {string}, get own property of `data.global.common_data`
+   * @param  {boolean=} options.negative if true, generate negative values as well (parallelling positive values)
+   */
+  function generateCounts(transforms, options={}) {
+    let arr = Util.arrayify(options.domain, data.global.common_data)
+    arr.forEach(function (ct) {
+      this.values.push({
+        name: (transforms.namefn) ? transforms.namefn.call(null, ct) : ct.toString(),
+        code: (transforms.codefn) ? transforms.codefn.call(null, ct) : ct.toString(),
+        use : (transforms.usefn)  ? transforms.usefn .call(null, ct) : '',
+      })
+    }, this)
+    if (options.negative) {
+      arr.forEach(function (i) {
         this.values.push({
-          name: (transforms.namefn) ? transforms.namefn.call(null, ct) : ct.toString(),
-          code: (transforms.codefn) ? transforms.codefn.call(null, ct) : ct.toString(),
-          use : (transforms.usefn)  ? transforms.usefn .call(null, ct) : '',
+          name: (transforms.namefn) ? transforms.namefn.call(null, -i) : (-i).toString(),
+          code: (transforms.codefn) ? transforms.codefn.call(null, -i) : `_${i}`,
+          use : (transforms.usefn ) ? transforms.usefn .call(null, -i) : '',
         })
       }, this)
-      if (options.negative) {
-        arr.forEach(function (i) {
-          this.values.push({
-            name: (transforms.namefn) ? transforms.namefn.call(null, -i) : (-i).toString(),
-            code: (transforms.codefn) ? transforms.codefn.call(null, -i) : `_${i}`,
-            use : (transforms.usefn ) ? transforms.usefn .call(null, -i) : '',
-          })
-        }, this)
-      }
     }
+  }
 
-    /**
-     * Automate spacing.
-     * NOTE: WARNING: STATEFUL FUNCTION (uses `data` parameter above).
-     * NOTE: METHOD FUNCTION. This function uses `this`, so must be called on an object.
-     * @param  {TransformObj} transforms a set of possible transformations
-     * @param  {!Object={}} options a set of possible options
-     * @param  {(number|Array<number>|string)=1} options.domain if {number}, use 1–n tracks; if {Array}, use those entries; if {string}, get own property of `data.global.common_data`
-     * @param  {boolean=} options.negative if true, generate negative values as well (parallelling positive values)
-     */
-    function generateSpaces(transforms, options={}) {
-      const ABBR = {
-        0.25: 'q'
-      , 0.5 : 'h'
-      }
-      let arr = Util.arrayify(options.domain, data.global.common_data)
+  /**
+   * Automate spacing.
+   * NOTE: WARNING: STATEFUL FUNCTION (uses `data` parameter above).
+   * NOTE: METHOD FUNCTION. This function uses `this`, so must be called on an object.
+   * @param  {TransformObj} transforms a set of possible transformations
+   * @param  {!Object={}} options a set of possible options
+   * @param  {(number|Array<number>|string)=1} options.domain if {number}, use 1–n tracks; if {Array}, use those entries; if {string}, get own property of `data.global.common_data`
+   * @param  {boolean=} options.negative if true, generate negative values as well (parallelling positive values)
+   */
+  function generateSpaces(transforms, options={}) {
+    const ABBR = {
+      0.25: 'q'
+    , 0.5 : 'h'
+    }
+    let arr = Util.arrayify(options.domain, data.global.common_data)
+    arr.forEach(function (sp) {
+      this.values.push({
+        name: (transforms.namefn) ? transforms.namefn.call(null, sp) : sp.toString(),
+        code: (transforms.codefn) ? transforms.codefn.call(null, sp) : `${ABBR[sp] || sp}`,
+        use : (transforms.usefn ) ? transforms.usefn .call(null, sp) : '',
+      })
+    }, this)
+    if (options.negative) {
       arr.forEach(function (sp) {
         this.values.push({
-          name: (transforms.namefn) ? transforms.namefn.call(null, sp) : sp.toString(),
-          code: (transforms.codefn) ? transforms.codefn.call(null, sp) : `${ABBR[sp] || sp}`,
-          use : (transforms.usefn ) ? transforms.usefn .call(null, sp) : '',
+          name: (transforms.namefn) ? transforms.namefn.call(null, -sp) : (-sp).toString(),
+          code: (transforms.codefn) ? transforms.codefn.call(null, -sp) : `_${ABBR[sp] || sp}`,
+          use : (transforms.usefn ) ? transforms.usefn .call(null, -sp) : '',
         })
       }, this)
-      if (options.negative) {
-        arr.forEach(function (sp) {
-          this.values.push({
-            name: (transforms.namefn) ? transforms.namefn.call(null, -sp) : (-sp).toString(),
-            code: (transforms.codefn) ? transforms.codefn.call(null, -sp) : `_${ABBR[sp] || sp}`,
-            use : (transforms.usefn ) ? transforms.usefn .call(null, -sp) : '',
+    }
+  }
+
+  /**
+   * Add values from a Value Type to a property.
+   * @param  {TransformObj} transforms a set of possible transformations
+   * @param  {!Object={}} options a set of possible options
+   * @param  {string=} options.name the name of the value type to inherit
+   */
+  function importValueType(transforms, options={}) {
+    this.values.push(...data.global.types.find((el) => el.name===options.name).values)
+  }
+
+  return (function () {
+    let ajv = new Ajv()
+    let isValid = ajv.compile(require('../bangs.schema.json'))
+    data.properties.forEach(function (property) {
+      (property.generators || []).forEach(function (generator) {
+        eval(generator.name).call(property, (function () {
+          let t = {}
+          ;['namefn','codefn','usefn'].forEach(function (key) {
+            t[key] = (generator.transforms && generator.transforms[key]) ? new Function(...generator.transforms[key]) : null
           })
-        }, this)
-      }
-    }
-
-    /**
-     * Add values from a Value Type to a property.
-     * @param  {TransformObj} transforms a set of possible transformations
-     * @param  {!Object={}} options a set of possible options
-     * @param  {string=} options.name the name of the value type to inherit
-     */
-    function importValueType(transforms, options={}) {
-      this.values.push(...data.global.types.find((el) => el.name===options.name).values)
-    }
-
-    return (function () {
-      let ajv = new Ajv()
-      let isValid = ajv.compile(require('../bangs.schema.json'))
-      data.properties.forEach(function (property) {
-        (property.generators || []).forEach(function (generator) {
-          eval(generator.name).call(property, (function () {
-            let t = {}
-            ;['namefn','codefn','usefn'].forEach(function (key) {
-              t[key] = (generator.transforms && generator.transforms[key]) ? new Function(...generator.transforms[key]) : null
-            })
-            return t
-          })(), generator.options)
-        })
+          return t
+        })(), generator.options)
       })
-      ;(function () {
-        let ajv = new Ajv()
-        let is_schema_valid = ajv.validateSchema(require('../bangs.schema.json'))
-        if (!is_schema_valid) {
-          console.error(ajv.errors)
-          throw new Error('Schema is not a valid schema!')
-        }
-      })()
-      ;(function () {
-        let ajv = new Ajv()
-        let is_data_valid = ajv.validate(require('../bangs.schema.json'), data)
-        if (!is_data_valid) {
-          console.error(ajv.errors)
-          throw new Error('Data does not valiate against schema!')
-        }
-      })()
-      return data
+    })
+    ;(function () {
+      let ajv = new Ajv()
+      let is_schema_valid = ajv.validateSchema(require('../bangs.schema.json'))
+      if (!is_schema_valid) {
+        console.error(ajv.errors)
+        throw new Error('Schema is not a valid schema!')
+      }
     })()
-  })(Util.cloneDeep(require('../bangs.json')))
+    ;(function () {
+      let ajv = new Ajv()
+      let is_data_valid = ajv.validate(require('../bangs.schema.json'), data)
+      if (!is_data_valid) {
+        console.error(ajv.errors)
+        throw new Error('Data does not valiate against schema!')
+      }
+    })()
+    return data
+  })()
+})(Util.cloneDeep(require('../bangs.json')))
 
 module.exports = class Bangs {
   /**
    * A set of static members used for the site.
    * Similar to a utility class.
-   * @constructor
    * @private
+   * @constructor
    */
   constructor() {}
 
